079e03e995110f3a07532499fbf74a0de925b310
IlyaOlkhovsky <ilianolhin@gmail.com>
2022-10-19 23:51:45 +0200

    just a commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import subprocess
+
+raw_data_dir = './raw_data'
+
+def get_raw_data():
+    
+
+
+def main():
+    get_raw_data()
+
+
+subprocess.run("python3 extract_tests.py {} {} {}".format(archive, '-o ' + dest, add_input),
+               shell=True, check=True)
+ 
